eeb0731a426a4b01c681afd22f06a5e7ee25cc1d
Vishnyakov.E.A <Vishnyakov.E.A@sbertech.ru>
2024-06-01 19:15:39 +0300

    Какая-то хрень с .sln

---
+++ IDM_user/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IDM;
+using IDM_user;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+Host.CreateDefaultBuilder()
+    .ConfigureLogging((context, logBuilder) => logBuilder.AddSimpleConsole())
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<Worker>();
+        services.AddIDMConnector(context.Configuration);
+    }).Build().Run();
---
+++ IDM_user/Worker.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using IDM.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
+
+namespace IDM_user;
+
+public class Worker : BackgroundService
+{
+    private readonly IConnector _connector;
+
+    public Worker(IConnector connector)
+    {
+        _connector = connector;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (true)
+        {
+            try
+            {
+                var positions = _connector.GetPositions();
+                var units = _connector.GetUnitsByParentId(1);
+                var employees = _connector.GetEmployeesByUnit(1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            await Task.Delay(10000, stoppingToken);
+        }
+    }
+}
